520e68a7308527f4ea580625754c6f0571de997b
Globlobglob <artyom14752@gmail.com>
2023-02-12 22:45:39 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] array = new int[5];
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            /*
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            /*
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(i );
+            }*/
+
+            /*
+            int[] array = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+            foreach (int elem in array)
+            {
+                Console.WriteLine(elem);
+            }
+            Console.ReadLine();*/
+
+
+        }
+    }
+}
